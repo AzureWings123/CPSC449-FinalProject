58d27474fd7df876f541ebc1d25d1720b44de6df
AzureWings123 <raganvanessa@gmail.com>
2026-04-27 10:48:35 -0700

    Initial commit

---
+++ fitnesslog/fitnesslog/src/main/java/com/example/fitnesslog/FitnesslogApplication.java
@@ -0,0 +1,13 @@
+package com.example.fitnesslog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitnesslogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitnesslogApplication.class, args);
+	}
+
+}
---
+++ fitnesslog/fitnesslog/src/test/java/com/example/fitnesslog/FitnesslogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fitnesslog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitnesslogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
