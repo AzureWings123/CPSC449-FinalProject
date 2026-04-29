c258164a1cfa79b8b67e2a89d19561b9a4d13d12
AzureWings123 <raganvanessa@gmail.com>
2026-04-29 09:38:09 -0700

    Dependencies and User
    User entity made + added some dependencies to pom.

--- fitnesslog/fitnesslog/src/main/java/com/example/fitnesslog/entity/User.java
+++ fitnesslog/fitnesslog/src/main/java/com/example/fitnesslog/entity/User.java
@@ -1,4 +1,20 @@
 package com.example.fitnesslog.entity;
 
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+@Document(collection = "users")
+@Data
 public class User {
+
+    @Id
+    private String id;
+
+    @Indexed(unique = true)
+    private String email;
+
+    private String password;
 }
