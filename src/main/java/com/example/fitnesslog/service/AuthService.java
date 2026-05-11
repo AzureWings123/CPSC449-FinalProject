6c1a8cc8661cf3f0742698df572807bd4236315d
sidvasu <siddharth.vasu.ca@gmail.com>
2026-05-11 14:15:56 -0700

    Fixed 401 for wrong email input

--- src/main/java/com/example/fitnesslog/service/AuthService.java
+++ src/main/java/com/example/fitnesslog/service/AuthService.java
@@ -41,7 +41,7 @@ public class AuthService {
 
     public AuthResponse login(LoginRequest request) {
         User user = userRepository.findByEmail(request.getEmail())
-                .orElseThrow(() -> new ResponseStatusException(HttpStatus.UNAUTHORIZED, "{ \n\"status\": 401, \n\"message\": Invalid credentials!\n}"));
+                .orElseThrow(() -> new AccessDeniedException("Invalid credentials!"));
 
 
         if (!passwordEncoder.matches(request.getPassword(), user.getPassword())) {
